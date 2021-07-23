ce71d52dca2edc9ec0ce4e37ad303bb52b597afc
= <martinkalvin6@gmail.com>
2021-07-23 15:49:26 +0100

    Added Vector and created the FlickAWP class

---
+++ src/main/java/com/csgo/Features/FlickAWP.java
@@ -0,0 +1,13 @@
+package com.csgo.Features;
+
+import com.csgo.Mem.Offsets;
+
+/**
+ * FlickAWP
+ */
+public class FlickAWP extends Offsets {
+
+    public static void Execute() {
+
+    }
+}
---
+++ src/main/java/com/csgo/Utils/Vector.java
@@ -0,0 +1,15 @@
+package com.csgo.Utils;
+
+public class Vector
+{
+    public Vector(float x, float y, float z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public float x;
+    public float y;
+    public float z;
+}
